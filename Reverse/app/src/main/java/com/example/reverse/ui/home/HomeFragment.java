ecf0e2eff8848ed92fee632c509c8b23880bc5c6
Ayarina <ayat.harastani@gmail.com>
2021-11-16 13:08:41 +0100

    Codigo de HomeFragment

--- Reverse/app/src/main/java/com/example/reverse/ui/home/HomeFragment.java
+++ Reverse/app/src/main/java/com/example/reverse/ui/home/HomeFragment.java
@@ -11,14 +11,27 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.reverse.FraseAdapter;
 import com.example.reverse.R;
+import com.example.reverse.TinyDB;
+import com.example.reverse.Usuario;
 import com.example.reverse.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+
+    private FraseAdapter fraseAdapter;
+    private TinyDB tinyDB;
+    private RecyclerView recyclerView;
+    private ArrayList<Object> frases;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,13 +41,21 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        //Inicializamos la base de datos
+        tinyDB = new TinyDB(getContext());
+        //Inicializamos el ArrayList (comprobando antes si esta vacío o no)
+        if(tinyDB.getListObject("key", Usuario.class) != null)
+            frases = tinyDB.getListObject("UsersData", Usuario.class);
+        else
+            frases = new ArrayList<>();
+        //Inicializamos el adaptador
+        fraseAdapter = new FraseAdapter(frases);
+
+        //Configuramos el RecyclerView con el UserAdapter como su controlador
+        recyclerView = (RecyclerView) root.findViewById(R.id.contact_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(fraseAdapter);
+
         return root;
     }
 
