58a6ea25217282105387891ebc3ab276464d3ef2
Ayarina <ayat.harastani@gmail.com>
2022-01-04 11:55:50 +0100

    Clase Usuario

--- Reverse/app/src/main/java/com/example/reverse/Usuario.java
+++ Reverse/app/src/main/java/com/example/reverse/Usuario.java
@@ -3,24 +3,26 @@ package com.example.reverse;
 import java.io.Serializable;
 
 public class Usuario implements Serializable {
-    private String nombre;
+    private String username;
     private String apellidos;
     private String numero;
+    private String correo;
 
     //Constructor
-    public Usuario(String nombre, String apellidos, String numero) {
-        this.nombre = (!nombre.trim().isEmpty()) ? nombre : " ";
+    public Usuario(String username, String apellidos, String numero, String correo) {
+        this.username = (!username.trim().isEmpty()) ? username : " ";
         this.apellidos = (!apellidos.trim().isEmpty()) ? apellidos : " ";
-        this.numero = (!nombre.trim().isEmpty()) ? numero : " ";
+        this.numero = (!numero.trim().isEmpty()) ? numero : " ";
+        this.correo = (!correo.trim().isEmpty()) ? correo : " ";
     }
 
     //Getters & Setters
-    public String getNombre() {
-        return nombre;
+    public String getUsername() {
+        return username;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getApellidos() {
@@ -39,4 +41,11 @@ public class Usuario implements Serializable {
         this.numero = numero;
     }
 
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
